4c87a8794d81e84a97d57b194331c0e39059f8b2
Victor Souza <victor.augusto800@gmail.com>
2021-09-30 21:57:43 -0300

    Add navbar

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
+
 const Hero = () => {
   const data = useGlobalContext();
   return <h2>hero component</h2>;
